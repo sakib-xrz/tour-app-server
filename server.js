b4091ab0e11a21c15c977a2175aa1a2335ae5ea8
sakib-repliq <sakibul.islam@repliq.dev>
2023-09-25 17:43:00 +0600

    Database connected

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
+/* eslint-disable node/no-unsupported-features/es-syntax */
 require('dotenv').config();
+const mongoose = require('mongoose');
 
 const app = require('./app');
 
 const port = process.env.PORT || 5000;
+const uri = process.env.DATABASE_URI;
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}...`);
-});
+async function main() {
+  try {
+    await mongoose.connect(uri);
+    console.log('Database connection successful');
+
+    app.listen(port, () => {
+      console.log(`Application listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log('Failed to connect to the database', error);
+  }
+}
+
+main();
